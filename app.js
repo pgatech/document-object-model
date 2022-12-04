58b4922b77e30f9e891d3912b3969ad8ea140164
pgatech <prayogahakimm@gmail.com>
2022-12-04 18:42:57 +0700

    Document Object Model

---
+++ app.js
@@ -0,0 +1,13 @@
+let val;
+
+val = document;
+val = document.all;
+val = document.all[2];
+val = document.all.length;
+
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.URL;
+
+console.log(val);
